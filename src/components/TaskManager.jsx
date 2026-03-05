af48041365ed887d4467dda66b79828b7f2c05bb
Luis Pomare <luispomare92@gmail.com>
2026-03-04 22:19:30 -0500

    Create submit functionality

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,26 +1,40 @@
 import { useState } from 'react';
 
 function TaskManager() {
-  /*  const [taskList, setTaskList] = useState({
-    text: '',
-    id: '',
-  });
-*/
+  const [taskList, setTaskList] = useState([]);
+
   const [newTask, setNewTask] = useState('');
 
   function onChangeHandler(event) {
     setNewTask(event.target.value);
   }
 
+  function onSubmitHandler(event) {
+    event.preventDefault();
+    setTaskList((prevState) => [
+      ...prevState,
+      { text: [newTask], id: [Date.now()] },
+    ]);
+    setNewTask('');
+  }
+
   return (
     <>
-      <form>
+      <ol>
+        <li>{taskList.length ? taskList[0].id : ''}</li>
+      </ol>
+      <form onSubmit={onSubmitHandler}>
         <input
           type='text'
           value={newTask}
           placeholder='Enter the new task'
           onChange={onChangeHandler}
+          name='newTask'
         />
+
+        <button type='submit' disabled={!newTask}>
+          Add
+        </button>
       </form>
     </>
   );
