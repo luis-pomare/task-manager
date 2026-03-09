a0614bee68c4ae59707be65e4f276410e5fd1f7a
Luis Pomare <luispomare92@gmail.com>
2026-03-08 22:32:37 -0500

    Create TaskForm component with controlled input

--- src/features/tasks/components/TaskForm.jsx
+++ src/features/tasks/components/TaskForm.jsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+
 function TaskForm() {
+  const [newTask, setNewTask] = useState('');
+
   return (
     <form>
-      <p>This is going to be the TaskForm</p>
+      <p>Please enter a new task</p>
+      <input
+        type='text'
+        placeholder='new task'
+        value={newTask}
+        onChange={(event) => setNewTask(event.target.value)}
+      />
     </form>
   );
 }
