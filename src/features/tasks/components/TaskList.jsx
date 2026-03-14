4f250491bf51140ec2b1226190eadc93c45c082b
Luis Pomare <luispomare92@gmail.com>
2026-03-13 23:21:31 -0500

    Add task counter to task list

--- src/features/tasks/components/TaskList.jsx
+++ src/features/tasks/components/TaskList.jsx
@@ -3,6 +3,7 @@ import TaskItem from './TaskItem';
 function TaskList({ taskList, deleteTask }) {
   return (
     <ol id='list-container'>
+      <p>The current amound of tasks is: {taskList.length}</p>
       {taskList.map((task) => (
         <TaskItem key={task.id} task={task} deleteTask={deleteTask} />
       ))}
